74ed73b51d59b5e7822cccdea4a2f3f992879a87
Rohit Jaggi <rohit2059.be20@chitkara.edu.in>
2024-06-07 00:33:53 +0530

    Intial setup of Project

---
+++ src/main/java/com/gwu/go_with_us/GoWithUsApplication.java
@@ -0,0 +1,13 @@
+package com.gwu.go_with_us;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoWithUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoWithUsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gwu/go_with_us/GoWithUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gwu.go_with_us;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoWithUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
